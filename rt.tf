9e398da3f86f331c10572dd81e48172af5bd4f2f
merycicek <merycicek@gmail.com>
2020-11-13 10:32:13 +0000

    rename vpc

--- rt.tf
+++ rt.tf
@@ -1,33 +1,33 @@
-resource "aws_route_table" "vpc_fuchicorp" { 
-  vpc_id = "${aws_vpc.vpc_fuchicorp.id}" 
+resource "aws_route_table" "vpc_eks" { 
+  vpc_id = "${aws_vpc.vpc_eks.id}" 
   route { 
     cidr_block = "0.0.0.0/0" 
-    gateway_id = "${aws_internet_gateway.vpc_fuchicorp.id}" 
+    gateway_id = "${aws_internet_gateway.vpc_eks.id}" 
     } 
 }
 
 resource "aws_route_table_association" "pub1" { 
   subnet_id = "${aws_subnet.pub1.id}" 
-  route_table_id = "${aws_route_table.vpc_fuchicorp.id}" 
+  route_table_id = "${aws_route_table.vpc_eks.id}" 
 } 
 
 resource "aws_route_table_association" "pub2" { 
   subnet_id = "${aws_subnet.pub2.id}" 
-  route_table_id = "${aws_route_table.vpc_fuchicorp.id}" 
+  route_table_id = "${aws_route_table.vpc_eks.id}" 
 } 
 
 resource "aws_route_table_association" "pub3" { 
   subnet_id = "${aws_subnet.pub3.id}" 
-  route_table_id = "${aws_route_table.vpc_fuchicorp.id}" 
+  route_table_id = "${aws_route_table.vpc_eks.id}" 
 } 
 
 resource "aws_route_table_association" "pub4" {
   subnet_id = "${aws_subnet.pub4.id}"
-  route_table_id = "${aws_route_table.vpc_fuchicorp.id}"
+  route_table_id = "${aws_route_table.vpc_eks.id}"
 } 
 
 resource "aws_route_table" "nat_gw" { 
-  vpc_id = "${aws_vpc.vpc_fuchicorp.id}" 
+  vpc_id = "${aws_vpc.vpc_eks.id}" 
   route { 
     cidr_block = "0.0.0.0/0" 
     gateway_id = "${aws_nat_gateway.nat_gw.id}" 
