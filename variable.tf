363b9c7eb4e95928a510d16aa532d0c3668f0969
merycicek <merycicek@gmail.com>
2020-11-14 11:49:42 +0000

    ...

--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
+    # variable "s3_folder_project" {}
+    # variable "s3_folder_region" {}
+    # variable "s3_folder_type" {}
+    # variable "s3_tfstate_file" {}
+    # variable "s3_bucket" {}
+    # variable "region" {}
+    # variable "environment" {}
+
+
+
+
+
 
 
 variable "region" {}
